85d35bd49c86b1011bce2a30d80268e121b8d8a5
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-19 17:13:05 +0100

    Refactor view logic to controller logic

--- app.rb
+++ app.rb
@@ -14,7 +14,6 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+	@random_names = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
